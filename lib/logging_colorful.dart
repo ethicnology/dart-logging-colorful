21725789385c8a08a7c9c00badc32fdbf183a2f0
ethicnology <ethicnology@pm.me>
2023-09-19 11:14:44 +0200

    chore: exports

--- lib/logging_colorful.dart
+++ lib/logging_colorful.dart
@@ -3,6 +3,6 @@
 /// More dartdocs go here.
 library;
 
-export 'src/logging_colorful_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+export 'package:logging/logging.dart';
+export 'src/color.dart';
+export 'src/logging.dart';
