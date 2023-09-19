df254e6814a2d0641b2d8f89ecb8fb5f7dedf888
ethicnology <ethicnology@pm.me>
2023-09-19 11:15:46 +0200

    docs: provide an example

--- example/logging_colorful_example.dart
+++ example/logging_colorful_example.dart
@@ -1,6 +1,29 @@
 import 'package:logging_colorful/logging_colorful.dart';
 
 void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+  // Define your Logger configuration
+  Logger.root.level = Level.ALL;
+  Logger.root.onRecord.listen((record) {
+    print('${record.loggerName} - ${record.level.name}: ${record.message}');
+  });
+
+  // Initialize a Logger
+  var log = Logger('MyLogger');
+  log.info('parent logger is not colorized');
+
+// Initialize a LoggerColorful that will make your logger colorized
+  final colorLog = LoggerColorful(log);
+  colorLog.shout('shout');
+  colorLog.severe('severe');
+  colorLog.warning('warning');
+  colorLog.info('info');
+  colorLog.config('config');
+  colorLog.fine('fine');
+  colorLog.finer('finer');
+  colorLog.finest('finest');
+
+  // Change the Color for a Level
+  // (In this case we assign an AnsiColor to [Level.FINEST] which has no AnsiColor to remove the rainbow)
+  colorLog.colorLevel[Level.FINEST] = AnsiColor.backgroundGreen;
+  colorLog.finest('finest with color instead of rainbow');
 }
